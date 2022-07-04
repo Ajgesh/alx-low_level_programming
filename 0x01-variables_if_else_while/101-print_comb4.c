4d4339a01806c8b99953073a2adb20ed8696ac91
Ajgesh <ajiboyesola92@yahoo.com>
2022-07-04 09:08:19 -0700

    101-print

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: always return o
+ */
+int main(void)
+{
+	int i;
+		int j;
+		int k;
+
+		for (i = 48; j < 58; i++)
+		{
+			for (j = i; j < 58; j++)
+			{
+				for (k = j; k < 58; k++)
+				{
+					if (i == j || j == k || i == k)
+					{
+						continue;
+					}
+					putchar (i);
+					putchar (j);
+					putchar (k);
+					if (i == 55 && j == 56 && k == 57)
+					{
+						break;
+					}
+					else
+					{
+						putchar (',');
+						putchar (' ');
+					}
+				}
+			}
+		}
+		putchar ('\n');
+		return (0);
+}
