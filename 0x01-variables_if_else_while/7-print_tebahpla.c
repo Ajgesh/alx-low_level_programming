7a1802da76bba45d400eb3f1d2b127b9a4650e4e
Ajgesh <ajiboyesola92@yahoo.com>
2022-07-04 06:48:25 -0700

    7-print

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: always return 0
+ */
+int main(void)
+{
+	char a;
+
+	for (a = 'z'; a >= 'a'; a--)
+	putchar (a);
+	puchar ('\n');
+	return (0);
+}
