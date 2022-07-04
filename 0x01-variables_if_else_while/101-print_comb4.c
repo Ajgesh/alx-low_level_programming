081c1542eba0df1c408e9a995ed24ccfeadfc8fc
Ajgesh <40598406+Ajgesh@users.noreply.github.com>
2022-07-04 21:27:21 +0100

    101-print

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,45 +1,43 @@
 #include <stdio.h>
 
-/**
- * main - Entry point
- *
- * Return: always return o
- */
+* main - Prints 3 combination of numbers
+  *
+  * Return: Always (Success)
+  */
 int main(void)
 {
-	int i;
-		int j;
-		int k;
-		int g;
+for (c = 48; c <= 57; c++)
+{
+for (i = 48; i <= 57; i++)
+{
+for (k = 48; k <= 57; k++)
+{
+for (j = 48; j <= 57; j++)
+{
+if (((k + j) > (c + i) &&  k >= c) || c < k)
+{
+putchar(c);
+putchar(i);
+putchar(' ');
+putchar(k);
+putchar(j);
 
-		for (i = 48; i <= 57; i++)
-		{
-			for (j = 48; j <= 57; j++)
-			{
-				for (k = 48; k <= 57; k++)
-				{
-					for (g = 48; g <= 57; g++)
-					{
-					if (((i + j) > (g + k) && i > g) || g < i)
-					{
-					putchar (i);
-					putchar (j);
-					putchar (' ');
-					putchar (g);
-					putchar (k);
-					if (i + j + k + g == 227 && i == 57
-					{
-						break;
-					}
-					else
-					{
-						putchar (',');
-						putchar (' ');
-					}
-					}
-					}
-				}
-		}
-		putchar ('\n');
-		return (0);
+if (c + i + k + j == 227 && c == 57)
+{
+break;
+}
+else
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+}
+}
+}
+
+putchar ('\n');
+
+return (0);
 }
