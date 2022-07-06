c3d90ec3babbea87950ff99ffc13be646f68ac8e
Ajgesh <ajiboyesola92@yahoo.com>
2022-07-05 19:02:07 -0700

    nested loop

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,7 +2,7 @@
 
 /**
  * print_alphabet_x10 - function to print abc 10 times
- *    
+ *
  * Return: 0
  */
 
@@ -13,19 +13,11 @@ void print_alphabet_x10(void)
 		int coun = 0;
 
 			char alpha;
-
-
-
-				while (coun++ <= 9)
-
-						{
-
-									for (alpha = 97; alpha <= 122; alpha++)
-
-													_putchar(alpha);
-
-											_putchar('\n');
-
-												}
+			while (coun++ <= 9)
+			{
+				for (alpha = 97; alpha <= 122; alpha++)
+					_putchar(alpha);
+				_putchar('\n');
+			}
 
 }
