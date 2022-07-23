68ff365064636459053b80108bbe2a761623aa43
Ajgesh <40598406+Ajgesh@users.noreply.github.com>
2022-07-23 04:23:37 +0100

    Update 1-strncat.c

--- 0x09-static_libraries/1-strncat.c
+++ 0x09-static_libraries/1-strncat.c
@@ -1,24 +1,35 @@
 #include "main.h"
 
 /**
- * _strncat - concatenates two strings
- * @src: The source of strings
- * @dest: The destination of the string
- * @n: The length of int
- * Return: pointer to the resulting string dest
+ * _strncat - concatenate two strings
+ * @dest: the string to append to
+ * @src: the string to append
+ * @n: the maximum number of bytes to append from src
+ *
+ * Description: This function appends the string pointed to by src to the
+ * string pointed to by dest, overwriting the null byte at the end of dest,
+ * and then adding a terminating null byte, using most n bytes from src.
+ *
+ * Return: a pointer to the destination string dest
  */
 char *_strncat(char *dest, char *src, int n)
 {
-	int i, j;
+char *dest_end = dest;
+int src_len = 0;
 
-	for (i = 0; dest[i] != '\0'; i++)
-	{
-		continue;
-	}
-	for (j = 0; src[j] != '\0' && j < n; j++)
-	{
-		dest[i + j] = src[j];
-	}
-	dest[i + j] = '\0';
-	return (dest);
+while (*dest_end)
+++dest_end;
+
+while (src_len < n && src[src_len])
+++src_len;
+
+if (src + src_len < dest || dest_end + src_len < src)
+{
+while (src_len--)
+*dest_end++ = *src++;
+*dest_end = '\0';
+
+}
+return (dest);
+
 }
